c52bc1e0f407757cf6d060006037dbf7d5c9db1a
Harikrish58 <harikrish61@gmail.com>
2025-03-08 11:34:47 +0100

    Callback hell task completed

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+  document.getElementById("container").innerHTML = "10";
+  setTimeout(() => {
+    document.getElementById("container").innerHTML = "9";
+    setTimeout(() => {
+      document.getElementById("container").innerHTML = "8";
+      setTimeout(() => {
+        document.getElementById("container").innerHTML = "7";
+        setTimeout(() => {
+          document.getElementById("container").innerHTML = "6";
+          setTimeout(() => {
+            document.getElementById("container").innerHTML = "5";
+            setTimeout(() => {
+              document.getElementById("container").innerHTML = "4";
+              setTimeout(() => {
+                document.getElementById("container").innerHTML = "3";
+                setTimeout(() => {
+                  document.getElementById("container").innerHTML = "2";
+                  setTimeout(() => {
+                    document.getElementById("container").innerHTML = "1";
+                    setTimeout(() => {
+                      document.getElementById("container").innerHTML = "💐🌹Happy Women's day💐🌹";
+                    },1000);
+                  },1000);
+                },1000);
+              },1000);
+            },1000);
+          },1000);
+        },1000);
+      },1000);
+    },1000);
+  },1000);
+},1000);
